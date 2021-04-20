9bf524d6660c1ce7ec0cf52c71b68522d90d1144
rakheoana <rakhew@gmail.com>
2021-04-20 22:08:33 +0200

    login on that front end navigation

--- public/includes/frontendheader.php
+++ public/includes/frontendheader.php
@@ -31,8 +31,18 @@
                                     <li class="nav-item"><a class="page-scroll" href="#contact">Contact</a></li>
                                     <li class="nav-item"><a class="page-scroll" href="#contact">Contact</a></li>
                                    
-                                </ul>
+                               
+
                             </div> 
+
+ <div class="collapse navbar-collapse">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item">
+                                        <a href="page-scroll" href="">login</a>
+                                    </li>
+                                </ul>
+ </div>
+                            
                         </nav> 
                     </div>
                 </div> 
