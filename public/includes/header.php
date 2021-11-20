53872734fb33942f9458c910fe9f8f37c7309647
rakheoana <rakhew@gmail.com>
2021-11-20 15:02:35 +0200

    fixed the hearder links  bug

--- public/includes/header.php
+++ public/includes/header.php
@@ -6,7 +6,7 @@ $root = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 $gpsarray = explode('/' ,$root);
 
-if(count($gpsarray) > 4 || count($gpsarray) < 2 )
+if(count($gpsarray) >= 4 || count($gpsarray) <= 2 )
 {
 
     if (in_array("authentication", $gpsarray))
