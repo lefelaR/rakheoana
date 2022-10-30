efb45dffdf6e318a5c19c910edadeaaf73173f72
Rakheoana <rakheoana@turati.com>
2022-10-30 20:26:40 +0200

    added bootstrap to project

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -13,13 +13,21 @@ const serverlessConfiguration: AWS = {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
+    stage:"dev",
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    deployment
+
   },
   // import the function via paths
-  functions: { hello },
+  functions: { 
+    ...hello,
+  },
+
+
+
   package: { individually: true },
   custom: {
     esbuild: {
